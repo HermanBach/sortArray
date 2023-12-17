9a75a302671be662472346bdf5db0c919faa874e
Герман Гашков <herman.gashkov@gmail.com>
2023-12-17 16:49:27 +0500

    Удалил php

--- 404.php
+++
@@ -1,5 +0,0 @@
-<?php
-header('Content-Type: text/html; charset=utf-8');
-echo "<link rel='stylesheet' href='style/style.css'>";
-echo "<p>Всё пропало! Ошибка 404!</p>";
-echo "<p><a href='index.html'>Вернуться обратно</a></p>";
